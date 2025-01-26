510dd92ba93b50db8818df4596d8e6faa54a8047
Abiy Addis <abiymw17@gmail.com>
2025-01-26 23:15:08 +0300

    Fresh migration files

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-
-Route::get('/user', function (Request $request) {
-
-    return DB::table('users')->first();
+use \App\Http\Controllers\UserController;
+Route::prefix('/v1/user')->group(function (){
+    Route::controller(UserController::class)->group(function (){
+        Route::post('/','postUser');
+        Route::get('/{id}','getUser');
+        Route::get('/{id}/post','getUserPost');
+        Route::get('/{id}/job','getUserJob');
+        Route::get('/{id}/complete','getAllUserCompleteRequest');
+    });
 });
