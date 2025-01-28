74d17d021ba1d3ac037732b9eeb634c3dc120c2a
Abiy Addis <abiymw17@gmail.com>
2025-01-28 20:36:10 +0300

    chat model update

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+    public function user1()
+    {
+        return $this->belongsTo(User::class, 'user_1_id');
+    }
+
+    // Define the relationship to the second user (user_2)
+    public function user2()
+    {
+        return $this->belongsTo(User::class, 'user_2_id');
+    }
 }
